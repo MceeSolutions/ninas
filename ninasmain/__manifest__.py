3d3c9ae7eac7dea664608c1be31951598423627f
steve <solise@mceesolutions.com>
2018-06-14 14:09:41 +0100

    ninas demo deployment

---
+++ ninasmain/__manifest__.py
@@ -0,0 +1,35 @@
+# -*- coding: utf-8 -*-
+
+#@author steve
+#Date: 4/06/18
+#'views/instructor_view.xml',
+{
+    'name': 'Ninas',
+    'summary': 'Ninas',
+    'Description': """
+                        Ninas
+                   """,
+    'author':'Mcee',               
+    'website': 'https://mceesolutions.com',
+    'category': 'Ninas',
+    'version':'11.0.0.0.0',
+    'application': True,
+    'insatallable': True,
+    'auto-install': False,
+        
+    'depends': ['base', 'hr', 'hr_holidays', 'hr_recruitment', 'website', 'stock', 'mail', 'sale', 'purchase', 'account_budget'],
+    'external_dependencies': {
+        'python': [],
+        'bin': []
+        },
+    'init_xml':[],
+    'data':[
+        'data/data.xml',
+        'views/stock_view.xml',
+        'views/views.xml'
+        ],
+    'css':[],
+    'demo_xml':[],
+    'test':[],            
+    
+    }
