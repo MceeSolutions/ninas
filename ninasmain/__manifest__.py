f5c1b4a23978ae32711ae713c54ce63664c932a3
steve <solise@mceesolutions.com>
2018-06-22 13:58:25 +0100

    ninas checklist

--- ninasmain/__manifest__.py
+++ ninasmain/__manifest__.py
@@ -12,12 +12,13 @@
     'author':'Mcee',               
     'website': 'https://mceesolutions.com',
     'category': 'ninas',
-    'version':'11.0.0.0.2',
+    'version':'11.0.0.0.3',
     'application': True,
     'insatallable': True,
     'auto-install': True,
         
-    'depends': ['base', 'hr', 'hr_holidays', 'hr_recruitment', 'website', 'stock', 'mail', 'sale', 'purchase', 'account_budget', 'hr_payroll', 'helpdesk', 'hr_expense'],
+    'depends': ['base', 'hr', 'hr_holidays', 'hr_recruitment', 'website', 'stock', 'mail', 'sale',
+                'purchase', 'account_budget', 'hr_payroll', 'helpdesk', 'hr_expense', 'hr_appraisal'],
     'external_dependencies': {
         'python': [],
         'bin': []
