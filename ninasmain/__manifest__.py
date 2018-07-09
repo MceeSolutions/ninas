d2e26e11ab020475cd2b4f9f65e2902fbdaaf4ea
steve <solise@mceesolutions.com>
2018-07-09 11:32:59 +0100

    application form

--- ninasmain/__manifest__.py
+++ ninasmain/__manifest__.py
@@ -12,7 +12,7 @@
     'author':'Mcee',               
     'website': 'https://mceesolutions.com',
     'category': 'ninas',
-    'version':'11.0.0.0.9',
+    'version':'11.0.0.0.10',
     'application': True,
     'insatallable': True,
     'auto-install': True,
--- ninasmain/models/application_form.py
+++ ninasmain/models/application_form.py
@@ -6,17 +6,23 @@ class ApplicationForm(models.Model):
     _inherit = ['mail.thread']
     
     name = fields.Char(
-        string='Applicant’s Authorized Representative’s Name')
+        string='Applicant’s Authorized Representative’s Name',
+        track_visibility='onchange')
     applicant_rep_title = fields.Selection([('mr','Mr.'),('ms', 'Ms.'), ('mrs','Mrs.'), ('dr','DR.'), ('engr','Engr.'), ('prof','Prof.')],
-        string='Authorized Representative’s Title')
+        string='Authorized Representative’s Title',
+        track_visibility='onchange')
     laboratory_legal_name = fields.Text(
-        string="Laboratory’s Legal Name")
+        string="Laboratory’s Legal Name",
+        track_visibility='onchange')
     laboratory_address= fields.Text(
-        string='Laboratory Address (Number and Street, City, State and Country)')
+        string='Laboratory Address (Number and Street, City, State and Country)',
+        track_visibility='onchange')
     telephone_number = fields.Char(
-        string='Telephone Numbers')
+        string='Telephone Numbers',
+        track_visibility='onchange')
     email_address = fields.Char(
-        string='Email Address(es) official for laboratory and alternate contact')
+        string='Email Address(es) official for laboratory and alternate contact',
+        track_visibility='onchange')
     mailing_address = fields.Char(
         string='Mailing Address (if different from the laboratory location – Number, Street, City, State, Country)')
     account = fields.One2many(
@@ -25,48 +31,64 @@ class ApplicationForm(models.Model):
         string="""
         Accounts: Enter the name, telephone number, and email address(es) of the staff member who will liaise with NiNAS
         for all financial matters.
-        """)
+        """, track_visibility='onchange')
     
     name_address_of_owners = fields.Text(
-        string='Name and Address of Owners (applicable in the case of private ownership)')
+        string='Name and Address of Owners (applicable in the case of private ownership)',
+        track_visibility='onchange')
     
     
     testing_lab = fields.Boolean(
-        string='ISO/IEC 17025 Testing Laboratory')
+        string='ISO/IEC 17025 Testing Laboratory', 
+        track_visibility='onchange')
     calibration_lab = fields.Boolean(
-        string='ISO/IEC 17025 Calibration Laboratory')
+        string='ISO/IEC 17025 Calibration Laboratory',
+        track_visibility='onchange')
     med_lab = fields.Boolean(
-        string='ISO 15189: Medical Laboratory')
+        string='ISO 15189: Medical Laboratory',
+        track_visibility='onchange')
     
     tertiary_level_lab = fields.Boolean(
-        string='Tertiary level lab/ Tests (>20)')
+        string='Tertiary level lab/ Tests (>20)',
+        track_visibility='onchange')
     secondary_level_lab = fields.Boolean(
-        string='Secondary level lab 11-20 Tests')
+        string='Secondary level lab 11-20 Tests',
+        track_visibility='onchange')
     primary_level_lab = fields.Boolean(
-        string='Primary level lab (1-10)')
+        string='Primary level lab (1-10)',
+        track_visibility='onchange')
     
     test = fields.One2many(
         comodel_name='test.method',
-        inverse_name='test_name')
+        inverse_name='test_name',
+        track_visibility='onchange')
     
     lab_capabilities=fields.Text(
-        string='Laboratory Capabilities:')
+        string='Laboratory Capabilities:', 
+        track_visibility='onchange')
     num_of_lab_staff=fields.Text(
-        string='Number of Laboratory Staff:')
+        string='Number of Laboratory Staff:', 
+        track_visibility='onchange')
     
     signed_by_authorized_rep = fields.Char(
-        string='Signed by Authorized Representative:')
+        string='Signed by Authorized Representative:',
+        track_visibility='onchange')
     place_and_date = fields.Char(
-        string='Place and date:')
+        string='Place and date:',
+        track_visibility='onchange')
     print_name_below = fields.Char(
-        string='Print Name below:')
+        string='Print Name below:',
+        track_visibility='onchange')
     
     telephone = fields.Char(
-        string='Telephone')
+        string='Telephone',
+        track_visibility='onchange')
     fax = fields.Char(
-        string='Fax')
+        string='Fax',
+        track_visibility='onchange')
     email = fields.Char(
-        string='Email')
+        string='Email',
+        track_visibility='onchange')
     
 class Accounts(models.Model):
     _name = 'application.account'
