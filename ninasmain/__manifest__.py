8946d721ca79a79f083ff5c21bed13641c367856
steve <solise@mceesolutions.com>
2018-07-25 15:26:25 +0100

    changed manifest

--- ninasmain/__manifest__.py
+++ ninasmain/__manifest__.py
@@ -12,7 +12,7 @@
     'author':'Mcee',               
     'website': 'https://mceesolutions.com',
     'category': 'ninas',
-    'version':'11.0.0.0.18',
+    'version':'11.0.0.0.19',
     'application': True,
     'insatallable': True,
     'auto-install': True,
