9fc56480ec619c8b87364f7b38fa002e3cb1ab30
steve <solise@mceesolutions.com>
2018-08-02 13:09:03 +0100

    accreditation updates

--- ninasmain/__manifest__.py
+++ ninasmain/__manifest__.py
@@ -12,7 +12,7 @@
     'author':'Mcee',               
     'website': 'https://mceesolutions.com',
     'category': 'ninas',
-    'version':'11.0.0.0.28',
+    'version':'11.0.0.0.29',
     'application': True,
     'insatallable': True,
     'auto-install': True,
