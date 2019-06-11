89c326417f108a299aa7e19ac15a037cdbef4700
steve <solise@mceesolutions.com>
2019-06-11 09:10:22 +0100

    recent updates

--- ninasmain/__manifest__.py
+++ ninasmain/__manifest__.py
@@ -12,7 +12,7 @@
     'author':'Mcee',               
     'website': 'https://mceesolutions.com',
     'category': 'ninas',
-    'version':'11.0.0.0.96',
+    'version':'11.0.0.0.97',
     'application': True,
     'insatallable': True,
     'auto-install': True,
