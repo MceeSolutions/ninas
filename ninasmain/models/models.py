9033221309d92cd705886afd31aff161fa3225c6
steve <solise@mceesolutions.com>
2019-11-21 14:06:23 +0100

    decision form fixed

--- ninasmain/models/models.py
+++ ninasmain/models/models.py
@@ -2529,7 +2529,8 @@ class DecisionForm(models.Model):
             self.application_id.button_approved_app()
             self.approved_by_ceo = True
         else:
-            self.application_id.stage_id = 18
+            self.approved_by_ceo = True
+            self.application_id.button_reject_appd()
     
     '''
     @api.multi
