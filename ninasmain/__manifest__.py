c841b36ba1daf1ae4dcca9824fbb52124c0be087
steve <solise@mceesolutions.com>
2020-01-09 14:29:22 +0100

    employee self service adjustment

--- ninasmain/__manifest__.py
+++ ninasmain/__manifest__.py
@@ -12,7 +12,7 @@
     'author':'Mcee',               
     'website': 'https://mceesolutions.com',
     'category': 'ninas',
-    'version':'11.0.0.0.150',
+    'version':'11.0.0.0.151',
     'application': True,
     'insatallable': True,
     'auto-install': True,
